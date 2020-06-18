4a6f3fdd0efe9130a0a232da34f0077c435bca98
Daniel Alves <danielalveslima36@gmail.com>
2020-06-18 19:18:36 -0300

    Implementação dos métodos para o cálculo do desvio padrão e do método getCompanies() que retorna todas as empresas com seus respctivos desvios

--- backend/grails-app/services/backend/CompanyService.groovy
+++ backend/grails-app/services/backend/CompanyService.groovy
@@ -9,7 +9,7 @@ class CompanyService {
 
     }
 
-    void getStocks(String company, int hours){
+    def getStocks(String company, int hours){
         //SET TIME
         long start = System.currentTimeMillis()
         Calendar date = Calendar.getInstance()
@@ -22,5 +22,45 @@ class CompanyService {
         print("\nSTOCK QUOTE: " + newStock.toString())
         print("\nTOTAL TIME: " + System.currentTimeMillis() - start)
         print("\nNUMBER OF QUOTES: " + newStock.size())
+    }
+
+    Double getMedia(List<Double> valor) {
+        try {
+            return getSoma(valor) / valor.size();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException("The list has null values");
+        }
+    }
+
+    Double getSoma(List<Double> valor) {
+        Double soma = 0;
+        for (int i = 0; i < valor.size(); i++) {
+            soma += valor.get(i);
+        }
+        return soma;
+    }
+
+    Double getDesvioPadrao(List<Double> valor) {
+        Double media = getMedia(valor);
+        int tam = valor.size();
+        Double desvPadrao = 0;
+        for (Double vlr : valor) {
+            Double aux = vlr - media;
+            desvPadrao += aux * aux;
+        }
+        return Math.sqrt(desvPadrao / (tam - 1));
+    }
+
+    def getCompanies(){
+        Map<String,Map> mapCompanies = [:]
+        def companies = Company.list()
+        for(Company company : companies){
+            Map mapCompany = [:]
+            mapCompany['Name'] = company.name
+            mapCompany['Segment'] = company.segment
+            mapCompany['Deviation'] = getDesvioPadrao(Stock.findAllByCompany(company)?.collect{it.price})
+            mapCompanies[company.id.toString()] = mapCompany
+        }
+        return mapCompanies
     }
 }
