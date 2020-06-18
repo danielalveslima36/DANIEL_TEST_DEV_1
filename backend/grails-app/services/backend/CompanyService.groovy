4ef94068d583939fd3b6332f48adecd9ce7eec91
Daniel Alves <danielalveslima36@gmail.com>
2020-06-18 00:08:30 -0300

    Implementação do método getStocks() que retorna todas as ações das ultimas X horas

---
+++ backend/grails-app/services/backend/CompanyService.groovy
@@ -0,0 +1,30 @@
+package backend
+
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class CompanyService {
+
+    def serviceMethod() {
+
+    }
+
+    void getStocks(String company, int hours){
+        //SET TIME
+        long start = System.currentTimeMillis()
+        Calendar date = Calendar.getInstance()
+        date.setTime(new Date())
+        date.add(Calendar.HOUR_OF_DAY, -hours)
+        //searching
+        def newCompany = Company.findByName(company)
+        def newStock = newCompany ? Stock.findAllByCompanyAndPriceDateBetween(newCompany, date.getTime(), new Date()): []
+
+        print("\nSTOCK QUOTE: " + newStock.toString())
+        print("\nTOTAL TIME: " + System.currentTimeMillis() - start)
+        print("\nNUMBER OF QUOTES: " + newStock.size())
+    }
+
+    def getCompanies(){
+        return Company.count();
+    }
+}
